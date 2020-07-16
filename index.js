ef916552692807b84eb54fe408ea3f7972fe88c9
weilonglin <randomfriend@live.nl>
2020-07-16 14:01:55 +0200

    First commit for ring exercise

---
+++ index.js
@@ -0,0 +1,8 @@
+const toTitleCase = require("to-title-case");
+console.log(toTitleCase);
+
+const lower = 'the lord of the rings: the return of the king';
+
+const title = toTitleCase(lower);
+
+console.log(title);
